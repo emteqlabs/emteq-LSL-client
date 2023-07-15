2d99147ad5be566f2db2f28395f74a5bae231dac
W4ltz <Walas.piotr.prywatny@gmail.com>
2023-07-15 22:17:23 +0100

    Working plotting and receiving LSL data

--- lslbackend.py
+++ lslbackend.py
@@ -5,29 +5,44 @@ from threading import Thread
 
 class Stream():
 
-    def __init__(self, name, source_id, lslType, callback):
-        self.name = name
-        self.source_id = source_id
-        self.lslType = lslType
+    def __init__(self, stream, callback):
+        print("opening streams")
+        self.stream = stream
+        self.name = stream.name()
+        self.source_id = stream.source_id()
+        self.lslType = stream.type()
         self.callback = callback
+        print(f"params set name: {self.name} type: {self.lslType}")
 
-        self.streams = resolve_stream(lslType, name)
         # create a new inlet to read from the stream
+        print(f"stream resolved {self.stream}")
 
-        self.inlet = pylsl.StreamInlet(streams[0])
+        self.inlet = pylsl.StreamInlet(self.stream)
 
+        print("starting thread")
+
+        self.workerRun = True
         self.streamWorker = Thread(target=self.__worker)
         self.streamWorker.start()
+
+        print("started thread")
 
 
     def __worker(self):
 
-        while True:
+        while self.workerRun:
             # get a new sample (you can also omit the timestamp part if you're not
             # interested in it)
             sample, timestamp = self.inlet.pull_sample()
 
             self.callback(sample, timestamp)
+
+    def close(self):
+        self.workerRun = False
+
+    def __del__(self):
+        self.workerRun = False
+
 
 class LSL:
 
@@ -43,7 +58,17 @@ class LSL:
             self.discoveredOutlets[stream.name()+stream.source_id()] = stream
             onName(stream.name(),stream.source_id())
 
-    def open(self,name,source_id,callback):
-        stream = self.discoveredOutlets[name+source_id]
-        self.openStreams[name+source_id] = Stream(stream.name(),stream.source_id(),stream.type(),callback)
+    def open(self,name,callback):
+        print(f"opening stream {name}")
+        if name in self.discoveredOutlets.keys():
+            print("stream found")
+            stream = self.discoveredOutlets[name]
+            
+            print("adding stream to open streams")
+            self.openStreams[name] = Stream(stream,callback)
+        else:
+            print(f"stream not found")
         pass
+
+    def close(self,name):
+        self.openStreams[name].close()
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from PyQt6 import QtWidgets  # Should work with PyQt5 / PySide2 / PySide6 as well
 import pyqtgraph as pg
 from lslbackend import LSL
+import numpy as np
 
 class App:
 
     def __init__(self):
 
+        self.bufferIdx = 0
+        self.bufferLength = 1000
+        self.plotBuffer = np.zeros((self.bufferLength))
         self.backend = LSL()
 
         self.app = QtWidgets.QApplication([])
@@ -16,8 +20,11 @@ class App:
         ## Create some widgets to be placed inside
         self.btn = QtWidgets.QPushButton('scan')
         # text = QtWidgets.QLineEdit('enter text')
-        self.listw = QtWidgets.QListWidget()
-        plot = pg.PlotWidget()
+        self.scannedOutlets   = QtWidgets.QListWidget()
+        self.connectedOutlets = QtWidgets.QListWidget()
+
+        self.plot = pg.PlotWidget()
+        self.data_line =  self.plot.plot(self.plotBuffer)
 
         self.btn.clicked.connect(self.buttonCallback)
 
@@ -27,8 +34,9 @@ class App:
 
         ## Add widgets to the layout in their proper positions
         layout.addWidget(self.btn, 0, 0)  # button goes in upper-left
-        layout.addWidget(self.listw, 2, 0)  # list widget goes in bottom-left
-        layout.addWidget(plot, 0, 1, 3, 1)  # plot goes on right side, spanning 3 rows
+        layout.addWidget(self.scannedOutlets, 2, 0)  # list widget goes in bottom-left
+        layout.addWidget(self.connectedOutlets, 3, 0)
+        layout.addWidget(self.plot, 0, 1, 3, 1)  # plot goes on right side, spanning 3 rows
         ## Display the widget as a new window
         w.show()
 
@@ -36,13 +44,44 @@ class App:
         self.app.exec()  # or app.exec_() for PyQt5 / PySide2
 
     def onName(self,name,source_id):
-        self.listw.addItem(name+source_id)
+        self.scannedOutlets.addItem(name+source_id)
+        self.scannedOutlets.itemClicked.connect(self.itemCallback)
 
     def buttonCallback(self):
         self.backend.scan(onName=self.onName)
 
         print("clicked")
 
+    def itemCallback(self,item):
+        print(f"itemCallback {item.text()}")
+
+        self.scannedOutlets.takeItem(self.scannedOutlets.row(item))
+        self.scannedOutlets.itemClicked.disconnect()
+
+        self.connectedOutlets.addItem(item.text())
+        self.connectedOutlets.itemClicked.connect(self.connectedItemCallback)
+
+        self.backend.open(item.text(),self.signalCallback)
+
+    def connectedItemCallback(self,item):
+        print(f"connectedItemCallback {item.text()}")
+        self.connectedOutlets.takeItem(self.connectedOutlets.row(item))
+        self.connectedOutlets.itemClicked.disconnect()
+
+        self.backend.close(item.text())
+
+        self.scannedOutlets.addItem(item.text())
+        self.scannedOutlets.itemClicked.connect(self.itemCallback)
+
+    def signalCallback(self,sample, timestamp):
+        # print(f"sample: {sample}, timestamp: {timestamp}")
+        self.plotBuffer[self.bufferIdx] = sample[0]
+        self.bufferIdx += 1
+        if self.bufferIdx >= self.bufferLength:
+            self.bufferIdx = 0
+
+        self.data_line.setData(self.plotBuffer)
+        pass
 
 if __name__ == "__main__":
     ## Always start by initializing Qt (only once per application)
